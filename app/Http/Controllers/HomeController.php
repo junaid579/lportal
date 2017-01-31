ef6425875aaffb89a30ba5e1a578ebbb837d39a2
Junaid khan <junaidkhan@Junaids-MacBook-Air.local>
2017-01-31 14:26:50 +0530

    first commit

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('home');
+    }
+
+
+    public function index_metro()
+    {
+        return view('home_metro');
+    }
+
+    public function index_metro_2()
+    {
+        return view('home_metro_2');
+    }
+
+
+}
